bee7c4e86689e11d1d2717cad9a73b41d9d8f4af
Luiz Henrique Almeida Gomes <113533787+luiz-henrique-ag@users.noreply.github.com>
2024-06-04 13:53:32 +0000

    build: add tamagui dependency and configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,92 +1,35 @@
-import '../global.css';
-import 'expo-dev-client';
-import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
-import { Icon } from '@roninoss/icons';
+// import '../tamagui-web.css'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import { Stack } from 'expo-router'
 
+import { useColorScheme } from 'react-native'
 
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-
-import { Link, Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, View } from 'react-native';
-
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-
-import { ThemeToggle } from '~/components/nativewindui/ThemeToggle';
-import { cn } from '~/lib/cn';
-import { useColorScheme, useInitialAndroidBarSync } from '~/lib/useColorScheme';
-import { NAV_THEME } from '~/theme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
+import { TamaguiProvider } from 'tamagui'
+import { tamaguiConfig } from '../tamagui.config'
 export default function RootLayout() {
-  useInitialAndroidBarSync();
-  const { colorScheme, isDarkColorScheme } = useColorScheme();
 
+  const colorScheme = useColorScheme()
   return (
-    <>
-      <StatusBar
-        key={`root-status-bar-${isDarkColorScheme ? 'light' : 'dark'}`}
-        style={isDarkColorScheme ? 'light' : 'dark'}
-      />
-       
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <BottomSheetModalProvider>
-        
-        
-        <ActionSheetProvider>
-        
-        <NavThemeProvider value={NAV_THEME[colorScheme]}>
-          <Stack screenOptions={SCREEN_OPTIONS}>
-            <Stack.Screen name="index" options={INDEX_OPTIONS} />
-            <Stack.Screen name="modal" options={MODAL_OPTIONS} />
-          </Stack>
-        </NavThemeProvider>
-        
-        </ActionSheetProvider>
-        
-        
-        </BottomSheetModalProvider>
-      </GestureHandlerRootView>
-      
-    </>
-  );
+
+    // add this
+
+    <TamaguiProvider config={tamaguiConfig}>
+
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+
+        <Stack>
+
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+
+        </Stack>
+
+      </ThemeProvider>
+
+    </TamaguiProvider>
+
+  )
+
 }
-
-const SCREEN_OPTIONS = {
-  animation: 'ios', // for android
-} as const;
-
-const INDEX_OPTIONS = {
-  headerLargeTitle: true,
-  title: 'NativeWindUI',
-  headerRight: () => <SettingsIcon />,
-} as const;
-
-function SettingsIcon() {
-  const { colors } = useColorScheme();
-  return (
-    <Link href="/modal" asChild>
-      <Pressable className="opacity-80">
-        {({ pressed }) => (
-          <View className={cn(pressed ? 'opacity-50' : 'opacity-90')}>
-            <Icon name="cog-outline" color={colors.foreground} />
-          </View>
-        )}
-      </Pressable>
-    </Link>
-  );
-}
-
-const MODAL_OPTIONS = {
-  presentation: 'modal',
-  animation: 'fade_from_bottom', // for android
-  title: 'Settings',
-  headerRight: () => <ThemeToggle />,
-} as const;
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-  
-  
+  let plugins = [
+    [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './tamagui.config.ts',
+        logTimings: true,
+        disableExtraction: process.env.NODE_ENV === 'development',
+      },
+    ],
 
-  
-    plugins.push('react-native-reanimated/plugin');
-  
+  ];
+
+  plugins.push('react-native-reanimated/plugin');
 
   return {
-    
-      presets: [
-        ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-        'nativewind/babel',
-      ],
-    
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
     plugins,
   };
 };
